db802bc5c4946daa726b2c27d74e54fca31af0f9
SONALI PATEL <patelsonali123@yahoo.com>
2018-04-19 09:18:14 -0400

    Finished capturing image, processing it, getting model prediction and display on screen, and flash settings

--- VisionMLApp/VisionMLApp/Controller/CameraViewController.swift
+++ VisionMLApp/VisionMLApp/Controller/CameraViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 import AVFoundation
+import CoreML
+import Vision
 
+enum FlashState {
+    case off
+    case on
+}
 
 class CameraViewController: UIViewController {
 
@@ -24,6 +30,9 @@ class CameraViewController: UIViewController {
     var captureSession: AVCaptureSession!
     var cameraOutput: AVCapturePhotoOutput!
     var previewLayer: AVCaptureVideoPreviewLayer!
+    var photoData: Data?
+    
+    var flashcontrolState: FlashState = .off
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +46,8 @@ class CameraViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapCameraView))
+        tap.numberOfTapsRequired = 1
         captureSession = AVCaptureSession()
         captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
         
@@ -57,13 +68,79 @@ class CameraViewController: UIViewController {
                 previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
                 
                 cameraView.layer.addSublayer(previewLayer)
+                cameraView.addGestureRecognizer(tap)
                 captureSession.startRunning()
             }
         } catch {
             debugPrint(error)
         }
     }
+    
+    @objc func didTapCameraView(sender: UITapGestureRecognizer) {
+        let settings = AVCapturePhotoSettings()
+////        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+////        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType, kCVPixelBufferWidthKey as String: 160, kCVPixelBufferHeightKey as String: 160]
+//        settings.previewPhotoFormat = previewFormat
+        
+        settings.previewPhotoFormat = settings.embeddedThumbnailPhotoFormat
+        
+        if flashcontrolState == .off {
+             settings.flashMode = .off
+        } else {
+            settings.flashMode = .on
+        }
+        cameraOutput.capturePhoto(with: settings, delegate: self)
+    }
+    func resultsMethod(request: VNRequest, error: Error?) {
+        guard let results = request.results as? [VNClassificationObservation] else { return }
+        
+        for classification in results {
+            print(classification.identifier)
+            if classification.confidence < 0.5 {
+             self.identificationLbl.text = "I'm not sure what this is. Please try again."
+                self.confidenceLbl.text = ""
+                break
+            } else {
+                self.identificationLbl.text = classification.identifier
+                self.confidenceLbl.text = "CONFIDENCE: \(Int(classification.confidence * 100))%"
+                break
+            }
+        }
+    }
 
+    @IBAction func flashBtnWasPressed(_ sender: Any) {
+        switch flashcontrolState {
+            case .off:
+            flashBtn.setTitle("FLASH ON", for: .normal)
+            flashcontrolState = .on
+            
+            case.on:
+            flashBtn.setTitle("FLASH OFF", for: .normal)
+            flashcontrolState = .off
+            
+        }
+    }
+    
+}
 
+extension CameraViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            debugPrint(error)
+        } else {
+            photoData = photo.fileDataRepresentation()
+            
+            do {
+                let model = try VNCoreMLModel(for: SqueezeNet().model)
+                let request = VNCoreMLRequest(model: model, completionHandler: resultsMethod)
+                let handler = VNImageRequestHandler(data: photoData!)
+                try handler.perform([request])
+            } catch {
+               debugPrint(error)
+            }
+            let image = UIImage(data: photoData!)
+            self.capturedImageView.image = image
+        }
+    }
 }
 
